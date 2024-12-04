17179e83f35301c8b24c1c012cf4310ccc1456fe
Matilde Park <matilde.park@pm.me>
2024-12-04 13:04:02 -0500

    types: more insets

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,8 +21,6 @@ export type Note = Row & {
     content: string;
 }
 
-export type NoteInsert = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>;
-
 export type Conversation = Row & {
     content: string;
 }
@@ -31,6 +29,7 @@ export type Tag = Row & {
     name: string;
 }
 
+// join tables
 export type NoteTag = Row & {
     noteId: number;
     tagId: number;
@@ -45,3 +44,13 @@ export type ConversationTag = Row & {
     conversationId: number;
     tagId: number;
 }
+
+// inserts
+export type NoteInsert = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>;
+export type TaskInsert = Omit<Task, 'id' | 'createdAt' | 'updatedAt'>;
+export type AuthInsert = Omit<Auth, 'id' | 'createdAt' | 'updatedAt'>;
+export type ConversationInsert = Omit<Conversation, 'id' | 'createdAt' | 'updatedAt'>;
+export type TagInsert = Omit<Tag, 'id' | 'createdAt' | 'updatedAt'>;
+export type NoteTagInsert = Omit<NoteTag, 'id' | 'createdAt' | 'updatedAt'>;
+export type TaskTagInsert = Omit<TaskTag, 'id' | 'createdAt' | 'updatedAt'>;
+export type ConversationTagInsert = Omit<ConversationTag, 'id' | 'createdAt' | 'updatedAt'>;
