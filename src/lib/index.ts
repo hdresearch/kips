efe9eb6e49d8f2a18b39811f6dc1dab66a7c0f7f
Matilde Park <matilde.park@pm.me>
2024-12-04 13:03:55 -0500

    lib: add tag handling

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 import Database from 'better-sqlite3';
+import type { Database as DatabaseType } from 'better-sqlite3';
+import { Tag, TagInsert } from '../types/index.js';
 import { join } from 'path';
 import os from 'os';
 
@@ -8,4 +10,27 @@ export function getDb(init = false) {
     db.pragma('journal_mode = WAL');
     db.pragma('foreign_keys = ON');
     return db;
+}
+
+export function getTagsById(db: DatabaseType, tagIds: number[]) {
+    const tags = db.prepare<Partial<Tag>[]>(`SELECT * FROM tags WHERE id IN (${tagIds.join(',')})`).all();
+    return tags;
+}
+
+export function submitTags(db: DatabaseType, tags: string[]): number[] {
+    const tagIds = [];
+    for (const tag of tags) {
+        const tagResult = db.prepare<Partial<Tag>>(`SELECT id FROM tags WHERE name = @name`).get({name: tag});
+        const tagId = (tagResult as { id: number })?.id;
+        if (tagResult) {
+            tagIds.push(tagId);
+        } else {
+            const insertTag = db.prepare<TagInsert>(`INSERT INTO tags (name) VALUES (@name)`);
+            insertTag.run({name: tag});
+            const tagResult = db.prepare<Partial<Tag>>(`SELECT id FROM tags WHERE name = @name`).get({name: tag});
+            const tagId = (tagResult as { id: number }).id;
+            tagIds.push(tagId);
+        }
+    }
+    return tagIds;
 }
